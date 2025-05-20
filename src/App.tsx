126156ac6c96653bd845f1f42376665ed6066f24
Danilo Bleul <danilobleul2005@gmail.com>
2025-05-20 13:27:17 +0200

    Adding temporary authenticator to App module

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
@@ -37,4 +37,24 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+export default function App() {
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user?.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
+}

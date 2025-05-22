9dc39b284d68c0ebe7401074b2bdc955b4ab42ad
Danilo Bleul <danilobleul2005@gmail.com>
2025-05-22 11:01:41 +0200

    Re-Enabling the centered content; Creating cards for testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import '@aws-amplify/ui-react/styles.css';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 export default function App() {
 
@@ -13,21 +21,55 @@ export default function App() {
 
 
                 <div style = {{
-                    width: "100vw",
-                    height: "100vh"
+                    width: "90vw",
+                    height: "90vh"
                 }}>
-                    <Button onClick={signOut}>Logout</Button>
 
                     <Tabs defaultValue="data">
                         <TabsList>
                             <TabsTrigger value="data">Data</TabsTrigger>
                             <TabsTrigger value="controller">Controller</TabsTrigger>
+                            <TabsTrigger value="about">About</TabsTrigger>
                             {/*TODO Logo and Items*/}
+                            <Button onClick={signOut}>Logout</Button>
                         </TabsList>
                         <TabsContent value="data">
 
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>Data</CardTitle>
+                                    <CardDescription>Diagrams showing and filtering the measuring data</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    Content
+                                </CardContent>
+                            </Card>
+
                         </TabsContent>
                         <TabsContent value="controller">
+
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>Controller</CardTitle>
+                                    <CardDescription>Listing, modifying and deleting controllers</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    Content
+                                </CardContent>
+                            </Card>
+
+                        </TabsContent>
+                        <TabsContent value="about">
+
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>About</CardTitle>
+                                    <CardDescription>Telling the background of the project</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    Content
+                                </CardContent>
+                            </Card>
 
                         </TabsContent>
                     </Tabs>
